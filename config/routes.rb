7cf8084a4c3d36ecd800ed66a25973d131822005
Karen Hambro <khambro@gmail.com>
2016-01-25 15:40:24 -0800

    changes new user route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: "maps#index"
 
-  get "/users", to: "users#new", as: :new_user
+  get "/new-user", to: "users#new", as: :new_user
   post "/new-user", to: "users#create"
 
   get "/sign-out", to: "sessions#sign_out", as: :sign_out
