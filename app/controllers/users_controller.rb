3af189584163ba9b2f8abfe5c092b1698d20ced0
Karen Hambro <khambro@gmail.com>
2016-01-25 16:15:46 -0800

    adds password_confirmation to params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :username, :current_city, :bids_count)
+    params.require(:user).permit(:email, :password, :username, :current_city, :bids_count, :password_confirmation)
   end
 
 end
