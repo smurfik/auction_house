59485394ea859cd6920151e3adce0df259910e01
Chris Hintz <chrishintz42@gmail.com>
2016-01-27 00:44:05 -0800

    Added google search with predictive text

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,23 +1,96 @@
-var map;
-$(function() {
+function initialize() {
 
-  if (navigator === undefined) {
-    initializeMap(-34.1, 150.6);
-  } else {
-
-  navigator.geolocation.getCurrentPosition(function(position){
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    initializeMap(lat,lng);
-    });
-  }
-
-  var initializeMap = (function(lat,lng) {
-    var mapDiv = $("#map")[0];
-    var map = new google.maps.Map(mapDiv, {
-      center: {lat: lat, lng: lng},
-      zoom: 13
-      });
+  var markers = [];
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-});
+  var defaultBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(-33.8902, 151.1759),
+      new google.maps.LatLng(-33.8474, 151.2631));
+  map.fitBounds(defaultBounds);
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox((input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    } for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+}
+
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+
+// var map;
+// $(function() {
+//
+//   if (navigator === undefined) {
+//     initializeMap(-34.1, 150.6);
+//   } else {
+//
+//   navigator.geolocation.getCurrentPosition(function(position){
+//     var lat = position.coords.latitude;
+//     var lng = position.coords.longitude;
+//     initializeMap(lat,lng);
+//     });
+//   }
+//
+//   var initializeMap = (function(lat,lng) {
+//     var mapDiv = $("#map")[0];
+//     var map = new google.maps.Map(mapDiv, {
+//       center: {lat: lat, lng: lng},
+//       zoom: 13
+//       });
+//   });
+//
+//   var geocoder = new google.maps.Geocoder();
+//
+//   $("#submit").click(function(){
+//
+//   var address = $("#address").val();
+//     geocoder.geocode({'address': address }, function(results) {
+//       var lat = results[0].geometry.location.lat();
+//       var lng = results[0].geometry.location.lng();
+//       map.setCenter({lat: lat, lng: lng});
+//     });
+//   });
+// });
