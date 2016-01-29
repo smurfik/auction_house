90a87f9ab0323708b19accfb78257d824a13fe08
smurfik <smurfik@mlpinit.com>
2016-01-28 16:47:12 -0800

    add ajax to walkscore

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -68,49 +68,12 @@ function initialize() {
       bounds.extend(place.geometry.location);
     }
 
-
     map.fitBounds(bounds);
+
+    $.get("/walkscore", {address: input.value, lat: map.getCenter().lat(), lng: map.getCenter().lng()}, function(data) {
+      $("#placeholder").html(data.walkscore);
+    });
+
   });
 
-
-
-
 }
-
-// google.maps.event.addDomListener(window, 'load', initialize);
-
-
-// var map;
-// $(function() {
-//
-//   if (navigator === undefined) {
-//     initializeMap(-34.1, 150.6);
-//   } else {
-//
-//   navigator.geolocation.getCurrentPosition(function(position){
-//     var lat = position.coords.latitude;
-//     var lng = position.coords.longitude;
-//     initializeMap(lat,lng);
-//     });
-//   }
-//
-//   var initializeMap = (function(lat,lng) {
-//     var mapDiv = $("#map")[0];
-//     var map = new google.maps.Map(mapDiv, {
-//       center: {lat: lat, lng: lng},
-//       zoom: 13
-//       });
-//   });
-//
-//   var geocoder = new google.maps.Geocoder();
-//
-//   $("#submit").click(function(){
-//
-//   var address = $("#address").val();
-//     geocoder.geocode({'address': address }, function(results) {
-//       var lat = results[0].geometry.location.lat();
-//       var lng = results[0].geometry.location.lng();
-//       map.setCenter({lat: lat, lng: lng});
-//     });
-//   });
-// });
