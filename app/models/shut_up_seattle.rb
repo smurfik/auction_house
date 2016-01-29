801607c94c8699cc72b4382aed0e16d7afb25c46
Chris Hintz <chrishintz42@gmail.com>
2016-01-29 13:48:21 -0800

    Interpolate lat and lng into the get request

--- app/models/shut_up_seattle.rb
+++ app/models/shut_up_seattle.rb
@@ -7,7 +7,7 @@ class ShutUpSeattle
   end
 
   def self.find(lat, lng)
-    response = HTTParty.get("http://api.shutupseattle.com/score?latitude=47.654063&longitude=-122.357423")
+    response = HTTParty.get("http://api.shutupseattle.com/score?latitude=#{lat}&longitude=#{lng}")
     self.new response.parsed_response
   end
 end
