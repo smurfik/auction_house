4a04976d12e5a1feb71ad7be7dd464e85a04f3b7
smurfik <smurfik@mlpinit.com>
2016-01-29 15:24:26 -0800

    add bid now function

---
+++ app/assets/javascripts/bid.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('.form .bid-now-button').click(function(e) {
+    e.preventDefault();
+    $('.buy-now-button').hide();
+    $('.bid-form').addClass("styled");
+    $('.bid-now-button').addClass("styled-bid-button");
+
+    $(this).unbind("click");
+    $('.bid-now-button').click(function() {
+    });
+  });
+
+  $('.text .bid-now-button').click(function(e) {
+    e.preventDefault();
+    alert("Please sign in!");
+  });
+
+});
