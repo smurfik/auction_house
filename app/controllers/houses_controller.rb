e86eb0af00ca15bdbcfff1af332c6fc4a9be85d1
Chris Hintz <chrishintz42@gmail.com>
2016-01-28 16:39:11 -0800

    Return JSON for Zillow data in the HousesController

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,31 +1,8 @@
 class HousesController < ApplicationController
 
   def show
-    address = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19vnw1f09vv_82rpe&address=#{@street}&citystatezip=#{@city}#{@state}#{@zipcode}"
-    @zillow = Zillow.find(address)
-    render json: @zillow
-
-    @street         = params[:street]
-    @city           = params[:city]
-    @state          = params[:state]
-    @zipcode        = params[:zipcode]
-    @zestimate      = params[:zestimate]
-    @latitude       = params[:latitude]
-    @longitude      = params[:longitude]
-    @bedrooms       = params[:bedrooms]
-    @bathrooms      = params[:bathrooms]
-    @year_built     = params[:yearBuilt]
-    @year_updated   = params[:yearUpdated]
-    @lot_size       = params[:lotSizeSqFt]
-    @finishedsqft   = params[:finishedSqFt]
-    @floors         = params[:numFloors]
-    @heating_system = params[:heatingSystem]
-    @heating_source = params[:heatingSources]
-    @parking        = params[:parkingType]
-
-    @json           = response.to_json
-    # take params and enter address data into url
-    # show Json (render jason) -- make hash
+    @zillow = Zillow.find(params[:street], params[:city], params[:state], params[:zipcode])
+    render json: @zillow.to_json
   end
 
   def search
