973d6491ecd4068a3ffc4c8d0aaaca1a1ff776a3
smurfik <smurfik@mlpinit.com>
2016-02-03 16:59:19 -0800

    add method to house controller for placing pins

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,12 +1,9 @@
-
 class HousesController < ApplicationController
 
   def show
     @zillow = Zillow.find(params["street"], params["city"], params["state"])
     render json: @zillow.to_json
   end
-
-
 
   def walkscore
     @walkscore = Walkscore.find(params["address"], params["lat"], params["lng"])
@@ -17,4 +14,10 @@ class HousesController < ApplicationController
     @shutupseattle = ShutUpSeattle.find(params["lat"], params["lng"])
     render json: @shutupseattle.to_json
   end
+
+  def place_pins
+    @houses = House.where.not(bids_count: 0)
+    render json: @houses
+  end
+
 end
