7d28355f47373a4602ffb51bda750f24a870b9b4
smurfik <smurfik@mlpinit.com>
2016-02-05 10:03:29 -0800

    add e-mail settings to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = {host: "auction-houzz.herokuapp.com"}
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV["MAIL_USERNAME"],
+    password:             ENV["MAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
