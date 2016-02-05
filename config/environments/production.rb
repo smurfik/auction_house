e746c9a74e8b10900af4b1f8dbc3ac0cdafae6e9
smurfik <smurfik@mlpinit.com>
2016-02-05 10:14:13 -0800

    switch from gmail to SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,13 +3,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            ENV["MAIL_USERNAME"],
-    password:             ENV["MAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   # Settings specified here will take precedence over those in config/application.rb.
 
